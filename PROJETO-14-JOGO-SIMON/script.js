94dcbbcea772e728cad9fc679df8b5849c3db080
Lucas Messias <100249992+Lukones@users.noreply.github.com>
2022-08-31 11:23:39 +0100

    Update script.js

--- PROJETO-14-JOGO-SIMON/script.js
+++ PROJETO-14-JOGO-SIMON/script.js
@@ -3,11 +3,15 @@ var buttonColourse = ["red", "blue", "green", "yellow"]
 var gamePattern = [];
 var userClickedPattern = [];
 
-$('.btn').click(function() {
+var started = false;
+var level = 0;
+
+$('.btn').click(function () {
     var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
-    animatePress(userChosenColour)
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
 function playSound(name) {
@@ -17,30 +21,67 @@ function playSound(name) {
 
 
 function nextSequence() {
-    var randomNumber = Math.round(Math.random()* 3);
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.round(Math.random() * 3);
     var randomChosenColour = buttonColourse[randomNumber];
     gamePattern.push(randomChosenColour);
-    console.log(gamePattern)
+    console.log(gamePattern);
     $('#' + randomChosenColour).fadeOut(150).fadeIn(150);
-    playSound(randomChosenColour)
+    playSound(randomChosenColour);
+
 };
 
 function animatePress(currentColour) {
     $('#' + currentColour).addClass('pressed');
-    setTimeout(function(){ 
-        $('#' + currentColour).removeClass('pressed'); 
-}, 100);
+    setTimeout(function () {
+        $('#' + currentColour).removeClass('pressed');
+    }, 100);
+
 }
 
-$(document).keypress('click', function(event){
-
-    
-    var a = event.key
-    if (a != false ) {
-        $('h1').text('Level 0')
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
     }
-    
-    
-})
+});
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log('CERTO!');
+        if (userClickedPattern.length === gamePattern.length) {
+            console.log('CERTO!');
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+
+
+        startOver();
+        playSound("wrong");
+
+        $('body').addClass('game-over');
+        setTimeout(function () {
+            $('body').removeClass('game-over');
+        }, 200);
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+
+        console.log('ERRO!');
+    }
+}
+
+function startOver() {
+    level = 0;
+    started = false;
+    gamePattern = [];
+}
 
 
